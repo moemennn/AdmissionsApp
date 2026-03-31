4d8c7e88d0d9fc385ace55475357365316fd8994
tshi1 <tshi1@fandm.edu>
2026-03-31 14:53:34 -0400

    Linked up buttons on landing page

--- app/src/main/java/edu/fandm/mibrahi1/admissionsapp/MainActivity.java
+++ app/src/main/java/edu/fandm/mibrahi1/admissionsapp/MainActivity.java
@@ -1,6 +1,8 @@
 package edu.fandm.mibrahi1.admissionsapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,7 +22,21 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        // Test commit - Tommie
-        // Test Commit - Kevin
+        Button startTour = (Button) findViewById(R.id.btnStartTour);
+        startTour.setOnClickListener(v -> {
+            NavigationHelper.startActivity(this, MainActivity.class);
+
+        });
+
+
+        Button bookVisit = (Button) findViewById(R.id.btnBookVisit);
+        bookVisit.setOnClickListener(v -> {
+            NavigationHelper.startActivityWithURL(this, WebPage.class, "https://www.fandm.edu/visit/");
+        });
+
+        Button meetCounselor = (Button) findViewById(R.id.btnMeetCounselor);
+        meetCounselor.setOnClickListener(v -> {
+            NavigationHelper.startActivity(this, AdmissionCounselor.class);
+        });
     }
 }
