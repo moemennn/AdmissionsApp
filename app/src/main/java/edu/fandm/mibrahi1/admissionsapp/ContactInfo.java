9b41d4c61076c7b56285661b82f82ff98491262a
mibrahi1 <mibrahi1@fandm.edu>
2026-04-02 15:26:57 -0400

    Added contact info activity

--- app/src/main/java/edu/fandm/mibrahi1/admissionsapp/ContactInfo.java
+++ app/src/main/java/edu/fandm/mibrahi1/admissionsapp/ContactInfo.java
@@ -1,6 +1,9 @@
 package edu.fandm.mibrahi1.admissionsapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,28 @@ public class ContactInfo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_contact_info);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.contact_layout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button btnEmail = findViewById(R.id.btnEmail);
+        Button btnPhone = findViewById(R.id.btnPhone);
+
+        // 📧 Email Admissions
+        btnEmail.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_SENDTO);
+            intent.setData(Uri.parse("mailto:admission@fandm.edu"));
+            intent.putExtra(Intent.EXTRA_SUBJECT, "Inquiry about F&M Admissions");
+            startActivity(intent);
+        });
+
+        // 📞 Call Admissions (default number)
+        btnPhone.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_DIAL);
+            intent.setData(Uri.parse("tel:7173583951")); // you can switch to the other number if you want
+            startActivity(intent);
         });
     }
 }
